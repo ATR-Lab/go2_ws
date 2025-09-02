a04c72dfe00958e217d3fa246748821d758f2820
Irvin | OpenDive <irvsteve@gmail.com>
2025-09-02 15:29:58 -0400

    Fix detecting launch files

--- src/navigation_manager/setup.py
+++ src/navigation_manager/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import glob
 
 package_name = 'navigation_manager'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob.glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
